d3b3433f89f5809fec37472b0d74e72aa64e6ecf
Cassio Lima <cassio.araujo.lima@gmail.com>
2024-09-04 21:48:35 -0300

    criação do app usuario

---
+++ users_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
