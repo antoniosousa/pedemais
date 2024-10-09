c126257e89abcd7cbd1f1fa3e33937fb7c00a7f2
Cassio Lima <cassio.araujo.lima@gmail.com>
2024-10-09 20:52:53 -0300

    unir tabelas de usuarios

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
+class User(AbstractUser):
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.name
+        return self.username
